fe408ab6b1ca55a6dca67a2cf739aefa842c4f3f
Olme-xD <olmem.molina02@gmail.com>
2025-10-13 13:19:37 -0400

    Included default files for API

---
+++ backend-api/src/main/java/com/example/SpartanMarketplace/SpartanMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpartanMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartanMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanMarketplaceApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/example/SpartanMarketplace/SpartanMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpartanMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartanMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
