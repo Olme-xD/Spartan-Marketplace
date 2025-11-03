7ddc46ed65fb0159a87dbc8e2788511808a4c0f1
Olme-xD <olmem.molina02@gmail.com>
2025-11-03 11:39:42 -0500

    deleted redundant dir Listings

--- backend-api/src/main/java/com/example/SpartanMarketplace/Listing/ListingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SpartanMarketplace.Listing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ListingRepository extends JpaRepository<Listing, Long> {
-}
